80168265416b2f65d4b8ee868bb09114f06494bf
GijsProductions <gijs.kempen@tip.nl>
2020-01-31 23:44:27 +0100

    Eerste opzet character controller, dit werkt nu basaal naar links rects en jump met spatie ;-) Woohoo!

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    private Rigidbody _rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //links en rechts ophalen
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        //jump ophalen
+        if(Input.GetKey(KeyCode.Space))
+        {
+            _rigidBody.AddForce(Vector3.up * 2.0f, ForceMode.Impulse);
+        }
+
+        //
+       Vector3 nieuwePositie = transform.position + (transform.right * moveInput) * 0.3f;
+
+       _rigidBody.MovePosition(nieuwePositie);
+    }
+}
