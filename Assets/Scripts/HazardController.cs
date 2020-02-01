6f896392f7f7d48a7a7392d9e6178567eb82f423
Tessa <tessa@gamebasics.nl>
2020-02-01 21:22:28 +0100

    updated main scene

--- Assets/Scripts/HazardController.cs
+++ Assets/Scripts/HazardController.cs
@@ -5,7 +5,7 @@ public class HazardController : MonoBehaviour
     public void OnCollisionEnter(Collision collision)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (collision.gameObject.name == "MainCharacter")
+        if (collision.gameObject.name == "character")
         {
             var bodyPartsController = collision.gameObject.GetComponent<BodyPartsSpawnController>();
             bodyPartsController.SpawnBodyParts();
