f6c58d717f9fa91c0a75f727b3a85bd28414afb2
GijsProductions <gijs.kempen@tip.nl>
2020-02-01 11:18:20 +0100

    jump is nu mooier

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -6,8 +6,9 @@ public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] float speed = 0.3f;
     [SerializeField] float jumpFactor = 8.0f;
-    private bool airTime;
+    [SerializeField] float jumpLeftRightFactor = 0.3f;
 
+    private bool _airTime;
     private Rigidbody _rigidBody;
 
     // Start is called before the first frame update
@@ -24,46 +25,36 @@ public class CharacterMovement : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
 
         //als er niet gejumpt wordt links of rechts wandelen
-        if (!airTime)
+        if (!_airTime)
         {
             Vector3 nieuwePositie = transform.position + (transform.right * moveInput) * speed;
             _rigidBody.MovePosition(nieuwePositie);
+            //if jump dan springen
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidBody.AddForce(Vector3.up * jumpFactor, ForceMode.Impulse);
+                _airTime = true;
+            }
         }
         //als er wel gejumped wordt links en rechts als impuls verwerken
-        if(airTime)
+        if(_airTime)
         {
-            ////sprong naar links verwerken
-            //if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-            //{
-            //    _rigidBody.AddForce(Vector3.left * jumpFactor, ForceMode.Impulse);
-            //}
-            ////sprong naar rechts verwerken
-            //if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            //{
-            //    _rigidBody.AddForce(Vector3.right * jumpFactor, ForceMode.Impulse);
-            //}
+            //sprong naar links verwerken
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                _rigidBody.AddForce(Vector3.left * jumpLeftRightFactor, ForceMode.Impulse);
+            }
+            //sprong naar rechts verwerken
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                _rigidBody.AddForce(Vector3.right * jumpLeftRightFactor, ForceMode.Impulse);
+            }
         }
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         //er kan weer gestuurd worden
-        airTime = false;
-
-        //jump alleen mogelijk als je op een ondergrond staat
-        switch (collision.gameObject.tag)
-        {
-            case "Jump-OK":
-                //jump ophalen
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    _rigidBody.AddForce(Vector3.up * jumpFactor, ForceMode.Impulse);
-                    airTime = true;
-                }
-                break;
-            
-            default:
-                break;
-        }
+        _airTime = false;
     }
 }
