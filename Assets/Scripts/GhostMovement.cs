d9fe42e6d3304026d4a4389b221ee733b10a5806
GijsProductions <gijs.kempen@tip.nl>
2020-02-01 17:33:38 +0100

    Spookje bodyparts kan oppakken en laten vallen

--- Assets/Scripts/GhostMovement.cs
+++ Assets/Scripts/GhostMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GhostMovement : MonoBehaviour
 {
     [SerializeField] float speed = 0.7f;
-    [SerializeField] float maxSpeed = 3.5f;
 
     private Rigidbody _rigidBody;
-
+    private Rigidbody _bodyPart;
+    private bool holdBodypart;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,25 +20,49 @@ public class GhostMovement : MonoBehaviour
     void Update()
     {
         //beweging naar links
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-            {
-                _rigidBody.AddForce(Vector3.left * speed, ForceMode.Impulse);
-            }
-            //beweging naar rechts 
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            {
-                _rigidBody.AddForce(Vector3.right * speed, ForceMode.Impulse);
-            }
-            //beweging omhoog
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-            {
-                _rigidBody.AddForce(Vector3.up * speed, ForceMode.Impulse);
-            }
-            //beweging omlaag
-            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-            {
-                _rigidBody.AddForce(Vector3.down * speed, ForceMode.Impulse);
-            } 
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            _rigidBody.AddForce(Vector3.left * speed, ForceMode.Impulse);
+        }
+        //beweging naar rechts 
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            _rigidBody.AddForce(Vector3.right * speed, ForceMode.Impulse);
+        }
+        //beweging omhoog
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _rigidBody.AddForce(Vector3.up * speed, ForceMode.Impulse);
+        }
+        //beweging omlaag
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _rigidBody.AddForce(Vector3.down * speed, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //bodpart loslaten
+            holdBodypart = false;
+            _bodyPart.transform.parent = null;
+            _bodyPart.isKinematic = false;
+            _bodyPart.useGravity = true;
+        }
+    }
 
+        //oppakken van onderdelen
+        public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BodyPart")
+        {
+            if (!holdBodypart)
+            {
+                _bodyPart = collision.gameObject.GetComponent<Rigidbody>();
+                collision.gameObject.transform.parent = transform;
+                _bodyPart.velocity = Vector3.zero;
+                _bodyPart.angularVelocity = Vector3.zero;
+                _bodyPart.isKinematic = true;
+                holdBodypart = true;
+            }
+        }
     }
 }
