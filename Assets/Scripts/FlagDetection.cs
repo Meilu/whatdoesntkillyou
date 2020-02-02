6cb85d462a81c5493ca3b16ef3d37e5c4ae70482
Tessa <tessa@gamebasics.nl>
2020-02-02 02:42:19 +0100

    ik ga slapen

---
+++ Assets/Scripts/FlagDetection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FlagDetection : MonoBehaviour
+{
+    public GameObject bodyPuzzlePrefab;
+    private bool created; 
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.name == "character" && created == false)
+        {
+            Instantiate(bodyPuzzlePrefab, transform.position, Quaternion.identity);
+            created = true;
+        }
+    }
+}
