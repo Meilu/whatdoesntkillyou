304c331ace65104decfdb43f45b18c83d2b7f4dd
Tessa <tessa@gamebasics.nl>
2020-02-02 13:52:39 +0100

    Added finishdetection to spawnpointdetection.

---
+++ Assets/Scripts/FinishDetection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FinishDetection : MonoBehaviour
+{
+    public GameObject finishMenu;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.name == "character")
+        {
+            // show menu
+            finishMenu.SetActive(true);
+
+            // activate animation
+        }
+    }
+}
