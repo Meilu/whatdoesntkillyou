ce10856e91ef27adb3d8d0a29cc561417581c4bf
Jurgen Welschen <jurgen.welschen@gmail.com>
2020-02-02 15:33:48 +0100

    finish sound

--- Assets/Scripts/FinishDetection.cs
+++ Assets/Scripts/FinishDetection.cs
@@ -1,13 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class FinishDetection : MonoBehaviour
 {
     public GameObject finishMenu;
+    private AudioSource _audioSource;
+    public AudioClip finishSound;
+
+    private void Start()
+    {
+      _audioSource = GetComponent<AudioSource>();
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.transform.parent.name == "character")
         {
+            GameObject.Find("backgroundsound").GetComponent<AudioSource>().Stop();
+            _audioSource.PlayOneShot(finishSound, 2.0f);
+            
+            other.transform.parent.GetComponent<CharacterMovement>().backFlip();
             // show menu
             finishMenu.SetActive(true);
 
